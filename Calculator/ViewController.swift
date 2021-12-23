9fe4acb558f011593e8e762456fe60522f567687
JuliaHsiao <juliaxiao860508@gmail.com>
2021-12-24 01:56:24 +0800

    there is a lot of problem about 0

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -16,10 +16,15 @@ class ViewController: UIViewController {
     @IBOutlet var operateButtons: [UIButton]!
     @IBOutlet weak var textLabel: UILabel!
     
+    var output: Double = 0.0
+    var equation: String = ""
+    var decimal: Bool = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        textLabel.text = "uBitch"
+        textLabel.text = "0"
+        
         for i in 0...9{
             numberButtons[i].clipsToBounds = true
             numberButtons[i].layer.cornerRadius = numberButtons[i].bounds.height*0.5
@@ -33,7 +38,101 @@ class ViewController: UIViewController {
     
     
     @IBAction func numberButtonPress(_ sender: UIButton) {
-        textLabel.text = String(sender.tag)
+        let inputNumber = sender.tag
+        
+        if textLabel.text == "0"{
+            textLabel.text = String(inputNumber)
+            equation += String(inputNumber)
+        }else{
+            textLabel.text! += String(inputNumber)
+            equation += String(inputNumber)
+        }
+        
+        decimal = false
+        
+        
+    }
+    
+
+    @IBAction func operateButtonPress(_ sender: UIButton) {
+        let origText = textLabel.text
+        textLabel.text  = ""
+        
+        if equation == ""{
+            equation = "0.0"
+        }else {
+            if decimal == false{
+                equation += ".0"
+            }
+        }
+        
+        
+        
+        switch sender.tag{
+            
+        // AC
+        case 0:
+            textLabel.text = "0"
+            
+            equation = "0"
+        
+        // +/-
+        case 1:
+            if let origText = origText{
+                if origText.contains("-"){
+                    textLabel.text = origText.replacingOccurrences(of: "-", with: "")
+                }else if origText == "0"{
+                    textLabel.text = "0"
+                }else if origText != ""{
+                  textLabel.text = "-" + origText
+                }
+            }
+            
+            
+            
+            equation +=  "*-1.0"
+            
+        // %
+        case 2:
+            equation += "/100.0"
+        
+        // /
+        case 3:
+            equation += "/"
+            
+        // *
+        case 4:
+        
+            equation += "*"
+        // -
+        case 5:
+            equation += "-"
+            
+        // +
+        case 6:
+            equation += "+"
+        // =
+        case 7:
+            let expression = NSExpression(format: equation)
+            output = expression.expressionValue(with: nil, context: nil) as! Double 
+            textLabel.text = String(output)
+            print(equation)
+            equation = String(output)
+            decimal = true
+            
+            
+        // .
+        case 8:
+            equation += "."
+            decimal = true
+        default:
+            textLabel.text = "Warning!!!"
+            break
+            
+            
+        }
+        
+        
     }
     
 
