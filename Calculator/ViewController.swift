171ccfad90266d9fd1c843ef781243ab8560c960
JuliaHsiao <juliaxiao860508@gmail.com>
2021-12-23 12:26:53 +0800

    2021/12/23 12:26

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,11 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+    @IBOutlet var numberButtons: [UIButton]!
+    
+    @IBOutlet var operateButtons: [UIButton]!
+    @IBOutlet weak var textLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        textLabel.text = "uBitch"
+        for i in 0...9{
+            numberButtons[i].clipsToBounds = true
+            numberButtons[i].layer.cornerRadius = numberButtons[i].bounds.height*0.5
+        }
+        for i in 0...8{
+            operateButtons[i].clipsToBounds = true
+            operateButtons[i].layer.cornerRadius = operateButtons[i].bounds.height*0.5
+        }
         // Do any additional setup after loading the view.
     }
-
+    
+    
+    @IBAction func numberButtonPress(_ sender: UIButton) {
+        textLabel.text = String(sender.tag)
+    }
+    
 
 }
 
